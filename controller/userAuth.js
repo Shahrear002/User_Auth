aadd21916b116b0de157c9675958541f7ba859f2
Shahrear Rahman <shahrear@Shahrears-MacBook-Air.local>
2022-03-16 15:21:45 +0600

    user login route, login controller, login validation added

--- controller/userAuth.js
+++ controller/userAuth.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken')
 
 //Load input validation
 const validateRegisterInput = require('../validation/register')
+const validateLoginInput = require('../validation/login')
 
 const register = (req, res) => {
     const { errors, isValid } = validateRegisterInput(req.body)
@@ -37,4 +38,43 @@ const register = (req, res) => {
     }
 }
 
-module.exports = { register }
+const login = (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body)
+
+    if(!isValid) {
+        res.status(400).send(errors)
+    } else {
+        const username = req.body.username
+        const pass = req.body.password
+
+        User.findOne({ where: { username: username } }).then(user => {
+            if(!user) {
+                errors.username = 'User not found'
+                res.status(400).send(errors)
+            }
+
+            bcrypt.compare(pass, user.password, (err, isMatch) => {
+                if(isMatch) {
+                    const payload = {
+                        id: user.id,
+                        username: user.username,
+                        firstname: user.firstname,
+                        lastname: user.lastname
+                    }
+                    
+                    jwt.sign(payload, process.env.secretOrKey, { expiresIn: '600s' }, (error, token) => {
+                        res.status(200).send({ success: true, token: "Bearer " + token })
+                    })
+                } else {
+                    errors.password = 'Password incorrect'
+                    res.status(400).send(errors)
+                }
+            })
+        })
+    }
+}
+
+module.exports = { 
+    register,
+    login
+}
--- routes/userAuth.js
+++ routes/userAuth.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const userAuthController = require('../controller/userAuth')
 
 router.post('/register', userAuthController.register)
+router.post('/login', userAuthController.login)
 
 module.exports = router
